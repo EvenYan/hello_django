a99c0a3eb0e9c9036dab1d69864c47e598b5de82
even <EvenYan>
2019-08-24 16:07:56 +0800

    添加模型，注册到后台

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from contact.models import PeopelInfo
 
 # Register your models here.
+
+
+admin.site.register(PeopelInfo)
--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class PeopelInfo(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.PositiveIntegerField()
+    phone_number = models.CharField(max_length=11)
+
+    def __str__(self):
+        return self.name
